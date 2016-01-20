fd107a4412a9443a590d6baf2cfda7b433c8f81e
platon79 <platon79@online.no>
2016-01-20 22:32:36 +0100

    GalaxyAnimator!!

--- starsapi/src/main/java/org/starsautohost/starsapi/tools/GalaxyViewer.java
+++ starsapi/src/main/java/org/starsautohost/starsapi/tools/GalaxyViewer.java
@@ -8,6 +8,7 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Vector;
@@ -36,12 +37,12 @@ public class GalaxyViewer extends JFrame implements ActionListener, ChangeListen
 	private Parser p;
 	private HashMap<Integer, String> planetNames = new HashMap<Integer, String>();
 	private HashMap<Integer, Point> planetCoordinates = new HashMap<Integer, Point>();
-	private int maxY;
+	private int maxX = 0, maxY = 0;
 	private Settings settings;
 	protected Vector<Integer> friends = new Vector<Integer>();
 	
 	//UI
-	private RPanel universe = new RPanel();
+	protected RPanel universe = new RPanel();
 	private JButton hw = new JButton("HW");
 	private JTextField search = new JTextField();
 	private JCheckBox names = new JCheckBox("Names",true);
@@ -49,11 +50,12 @@ public class GalaxyViewer extends JFrame implements ActionListener, ChangeListen
 	private JButton help = new JButton("Help");
 	private JCheckBox colorize = new JCheckBox("Colorize",false);
 	private HashMap<Integer,Color> colors = new HashMap<Integer, Color>();
+	private boolean animatorFrame;
 	
 	public static void main(String[] args) throws Exception{
 		try{
 			Settings settings = Settings.init();
-			new GalaxyViewer(settings);
+			new GalaxyViewer(settings,false);
 		}catch(Exception ex){
 			ex.printStackTrace();
 			System.err.println(ex.toString());
@@ -62,7 +64,7 @@ public class GalaxyViewer extends JFrame implements ActionListener, ChangeListen
 		}
 	}
 	
-	private static class Settings{
+	protected static class Settings{
 		protected int playerNr = 0;
 		protected String directory = ".";
 		protected String gameName = "";
@@ -80,7 +82,7 @@ public class GalaxyViewer extends JFrame implements ActionListener, ChangeListen
 					String s = in.readLine();
 					if (s == null) break;
 					if (s.contains("=") == false) continue;
-					String[] el = s.split("=");
+					String[] el = s.split("=",-1);
 					if (el[0].equalsIgnoreCase("PlayerNr")) settings.playerNr = Integer.parseInt(el[1].trim())-1;
 					if (el[0].equalsIgnoreCase("GameName")) settings.gameName = el[1].trim();
 					if (el[0].equalsIgnoreCase("GameDir")) settings.directory = el[1].trim();
@@ -114,22 +116,28 @@ public class GalaxyViewer extends JFrame implements ActionListener, ChangeListen
 		}
 	}
 	
-	public GalaxyViewer(Settings settings) throws Exception{
+	public GalaxyViewer(Settings settings, boolean animatorFrame) throws Exception{
 		super("Stars GalaxyViewer");
 		this.settings = settings;
+		this.animatorFrame = animatorFrame;
 		if (settings.gameName.equals("")) throw new Exception("GameName not defined in settings.");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		File dir = new File(settings.directory);
 		File map = new File(dir,settings.getGameName()+".MAP");
 		if (map.exists() == false){
-			String error = "Could not find "+map.getAbsolutePath()+"\n";
-			error += "Export this file from Stars! (Only needs to be done one time pr game)";
-			throw new Exception(error);
+			File f = new File(dir.getParentFile(),settings.getGameName()+".MAP");
+			if (f.exists()) map = f;
+			else{
+				String error = "Could not find "+map.getAbsolutePath()+"\n";
+				error += "Export this file from Stars! (Only needs to be done one time pr game)";
+				throw new Exception(error);
+			}
 		}
 		Vector<File> mFiles = new Vector<File>();
 		Vector<File> hFiles = new Vector<File>();
 		for (File f : dir.listFiles()){
 			if (f.getName().toUpperCase().endsWith("MAP")) continue;
+			if (f.getName().toUpperCase().endsWith("HST")) continue;
 			if (f.getName().toUpperCase().startsWith(settings.getGameName()+".M")) mFiles.addElement(f);
 			else if (f.getName().toUpperCase().startsWith(settings.getGameName()+".H")) hFiles.addElement(f);
 		}
@@ -144,7 +152,7 @@ public class GalaxyViewer extends JFrame implements ActionListener, ChangeListen
 		
 		//UI:
 		JPanel cp = (JPanel)getContentPane();
-		setLayout(new BorderLayout());
+		cp.setLayout(new BorderLayout());
 		cp.add(universe,BorderLayout.CENTER);
 		JPanel south = createPanel(0,hw,new JLabel("Search: "),search,names,zoom,colorize);
 		search.setPreferredSize(new Dimension(100,-1));
@@ -157,7 +165,8 @@ public class GalaxyViewer extends JFrame implements ActionListener, ChangeListen
 		setSize(800,600);
 		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 		setLocation((screen.width-getWidth())/2, (screen.height-getHeight())/2);
-		setVisible(true);
+		setVisible(animatorFrame == false);
+		if (animatorFrame) names.setSelected(false);
 	}
 	
 	private void parseMapFile(File map) throws Exception{
@@ -172,6 +181,7 @@ public class GalaxyViewer extends JFrame implements ActionListener, ChangeListen
 			planetNames.put(id,el[3]);
 			int x = Integer.parseInt(el[1]);
 			int y = Integer.parseInt(el[2]);
+			if (x > maxX) maxX = x;
 			if (y > maxY) maxY = y;
 			planetCoordinates.put(id,new Point(x,y));
 		}
@@ -189,11 +199,13 @@ public class GalaxyViewer extends JFrame implements ActionListener, ChangeListen
 			}
 			postProcess();
 			
-			PlayerBlock player = players[settings.playerNr];
-			if (player != null){
-				for (PlayerBlock pb : players){
-					if (pb == null) continue;
-					if (player.getPlayerRelationsWith(pb.playerNumber) == 1) friends.addElement(pb.playerNumber);
+			if (settings.playerNr >= 0){ //Not set if called from GalaxyAnimator
+				PlayerBlock player = players[settings.playerNr];
+				if (player != null){
+					for (PlayerBlock pb : players){
+						if (pb == null) continue;
+						if (player.getPlayerRelationsWith(pb.playerNumber) == 1) friends.addElement(pb.playerNumber);
+					}
 				}
 			}
 		}
@@ -213,41 +225,49 @@ public class GalaxyViewer extends JFrame implements ActionListener, ChangeListen
     }
 	
 	private void calculateColors(){
-		Vector<PlayerBlock> friends = new Vector<PlayerBlock>();
-		Vector<PlayerBlock> enemies = new Vector<PlayerBlock>();
-		Vector<Color> friendlyColors = new Vector<Color>();
-		Vector<Color> enemyColors = new Vector<Color>();
-		friendlyColors.addElement(new Color(255,255,0));
-		friendlyColors.addElement(new Color(183,197,21));
-		friendlyColors.addElement(new Color(197,133,21));
-		friendlyColors.addElement(new Color(21,197,125));
-		friendlyColors.addElement(new Color(255,255,192));
-		friendlyColors.addElement(new Color(21,178,197));
-		
-		enemyColors.addElement(new Color(255,0,0));
-		enemyColors.addElement(new Color(255,92,92));
-		enemyColors.addElement(new Color(197,21,133));
-		enemyColors.addElement(new Color(255,64,64));
-		enemyColors.addElement(new Color(255,128,128));
-		enemyColors.addElement(new Color(255,0,255));
-		enemyColors.addElement(new Color(145,0,197));
-		enemyColors.addElement(new Color(112,34,34));
-		enemyColors.addElement(new Color(90,34,102));
-		enemyColors.addElement(new Color(230,126,56));
-		for (PlayerBlock pb : p.players){
-			if (pb == null) continue;
-			if (pb.playerNumber == settings.playerNr) continue;
-			if (pb.getPlayerRelationsWith(settings.playerNr) == 1) friends.addElement(pb);
-			if (friends.contains(pb.playerNumber)) friends.addElement(pb);
-			else enemies.addElement(pb);
+		if (animatorFrame){
+			List<Color> distinctColors = pickColors(16);
+			for (int t = 0; t < distinctColors.size(); t++){
+				colors.put(t,distinctColors.get(t));
+			}
 		}
-		for (int t = 0; t < enemies.size(); t++){
-			PlayerBlock pb = enemies.elementAt(t);
-			if (enemyColors.size() > 0) colors.put(pb.playerNumber, enemyColors.remove(0));
-		}
-		for (int t = 0; t < friends.size(); t++){
-			PlayerBlock pb = friends.elementAt(t);
-			if (friendlyColors.size() > 0) colors.put(pb.playerNumber, friendlyColors.remove(0));
+		else{
+			Vector<PlayerBlock> friends = new Vector<PlayerBlock>();
+			Vector<PlayerBlock> enemies = new Vector<PlayerBlock>();
+			Vector<Color> friendlyColors = new Vector<Color>();
+			Vector<Color> enemyColors = new Vector<Color>();
+			friendlyColors.addElement(new Color(255,255,0));
+			friendlyColors.addElement(new Color(183,197,21));
+			friendlyColors.addElement(new Color(197,133,21));
+			friendlyColors.addElement(new Color(21,197,125));
+			friendlyColors.addElement(new Color(255,255,192));
+			friendlyColors.addElement(new Color(21,178,197));
+			
+			enemyColors.addElement(new Color(255,0,0));
+			enemyColors.addElement(new Color(255,92,92));
+			enemyColors.addElement(new Color(197,21,133));
+			enemyColors.addElement(new Color(255,64,64));
+			enemyColors.addElement(new Color(255,128,128));
+			enemyColors.addElement(new Color(255,0,255));
+			enemyColors.addElement(new Color(145,0,197));
+			enemyColors.addElement(new Color(112,34,34));
+			enemyColors.addElement(new Color(90,34,102));
+			enemyColors.addElement(new Color(230,126,56));
+			for (PlayerBlock pb : p.players){
+				if (pb == null) continue;
+				if (pb.playerNumber == settings.playerNr) continue;
+				if (pb.getPlayerRelationsWith(settings.playerNr) == 1) friends.addElement(pb);
+				if (friends.contains(pb.playerNumber)) friends.addElement(pb);
+				else enemies.addElement(pb);
+			}
+			for (int t = 0; t < enemies.size(); t++){
+				PlayerBlock pb = enemies.elementAt(t);
+				if (enemyColors.size() > 0) colors.put(pb.playerNumber, enemyColors.remove(0));
+			}
+			for (int t = 0; t < friends.size(); t++){
+				PlayerBlock pb = friends.elementAt(t);
+				if (friendlyColors.size() > 0) colors.put(pb.playerNumber, friendlyColors.remove(0));
+			}
 		}
 	}
 	
@@ -259,7 +279,7 @@ public class GalaxyViewer extends JFrame implements ActionListener, ChangeListen
 		return null;
 	}
 	
-	private class RPanel extends JPanel{
+	protected class RPanel extends JPanel{
 		private static final long serialVersionUID = 1L;
 		private double zoom = 100.0;
 		private int mariginX = 0;
@@ -302,11 +322,18 @@ public class GalaxyViewer extends JFrame implements ActionListener, ChangeListen
 				int rad = 3;
 				if (planet != null){
 					rad = 6;
-					if (planet.owner == settings.playerNr) g.setColor(Color.green);
-					else if (colorize.isSelected() && colors.get(planet.owner) != null) g.setColor(colors.get(planet.owner));
-					else if (friends.contains(planet.owner)) g.setColor(Color.YELLOW);
-					else if (planet.owner >= 0) g.setColor(Color.red);
-					else rad = 3;
+					if (animatorFrame){
+						Color col = colors.get(planet.owner);
+						if (col == null) col = Color.gray;
+						g.setColor(col);
+					}
+					else{
+						if (planet.owner == settings.playerNr) g.setColor(Color.green);
+						else if (colorize.isSelected() && colors.get(planet.owner) != null) g.setColor(colors.get(planet.owner));
+						else if (friends.contains(planet.owner)) g.setColor(Color.YELLOW);
+						else if (planet.owner >= 0) g.setColor(Color.red);
+						else rad = 3;
+					}
 				}
 				int x = convertX(p.x);
 				int y = convertY(p.y);
@@ -323,13 +350,14 @@ public class GalaxyViewer extends JFrame implements ActionListener, ChangeListen
 					int stringWidth = g.getFontMetrics().stringWidth(name);
 					g.drawString(name, x-stringWidth/2, y+12);
 				}
-				if (colorize.isSelected()){
+				if (colorize.isSelected() || animatorFrame){
 					int yy = 20;
 					g.setFont(g.getFont().deriveFont((float)12));
 					for (PlayerBlock pb : GalaxyViewer.this.p.players){
 						if (pb == null) continue;
 						Color col = colors.get(pb.playerNumber);
-						if (pb.playerNumber == settings.playerNr) col = Color.green;
+						if (animatorFrame && col != null) ; //Ok
+						else if (pb.playerNumber == settings.playerNr) col = Color.green;
 						else if (col == null) col = Color.red;
 						g.setColor(col);
 						String n = new String(pb.nameBytes); //Does not work
@@ -363,6 +391,14 @@ public class GalaxyViewer extends JFrame implements ActionListener, ChangeListen
 			mariginY = (int)((y-(h/2.0)));
 			System.out.println("Planet: "+x+" "+y);
 			System.out.println("Marigin: "+mariginX+" "+mariginY);
+			repaint();
+		}
+
+		public void zoomToFillGalaxy() {
+			double ySize = maxY-1000;
+			zoom = 100.0 * getHeight() / ySize;
+			Point center = new Point((maxX-1000)/2+1000,(maxY-1000)/2+1000);
+			centerOnPoint(center);
 			repaint();
 		}
 	}
@@ -427,5 +463,49 @@ public class GalaxyViewer extends JFrame implements ActionListener, ChangeListen
 				}
 			}
 		}
+	}
+	
+	public static List<Color> pickColors(int num) {
+		List<Color> colors = new ArrayList<Color>();
+		if (num < 2)
+			return colors;
+		float dx = 1.0f / (float) (num - 1);
+		for (int i = 0; i < num; i++) {
+			colors.add(get(i * dx));
+		}
+		return colors;
+	}
+
+	public static Color get(float x) {
+		float r = 0.0f;
+		float g = 0.0f;
+		float b = 1.0f;
+		if (x >= 0.0f && x < 0.2f) {
+			x = x / 0.2f;
+			r = 0.0f;
+			g = x;
+			b = 1.0f;
+		} else if (x >= 0.2f && x < 0.4f) {
+			x = (x - 0.2f) / 0.2f;
+			r = 0.0f;
+			g = 1.0f;
+			b = 1.0f - x;
+		} else if (x >= 0.4f && x < 0.6f) {
+			x = (x - 0.4f) / 0.2f;
+			r = x;
+			g = 1.0f;
+			b = 0.0f;
+		} else if (x >= 0.6f && x < 0.8f) {
+			x = (x - 0.6f) / 0.2f;
+			r = 1.0f;
+			g = 1.0f - x;
+			b = 0.0f;
+		} else if (x >= 0.8f && x <= 1.0f) {
+			x = (x - 0.8f) / 0.2f;
+			r = 1.0f;
+			g = 0.0f;
+			b = x;
+		}
+		return new Color(r, g, b);
 	}
 }

c22707944ab885d769e857261e61f42d9eb00078
platon79 <platon79@online.no>
2016-01-20 20:49:07 +0100

    version 0.1 of GalaxyViewer being published :-)

---
+++ synchelp.bat
@@ -0,0 +1,9 @@
+@echo off
+echo https://help.github.com/articles/fork-a-repo/
+git remote -v
+echo Sync help. Type:
+echo git fetch upstream
+echo git checkout master
+echo git merge upstream/master
+echo commit changes..
+echo git push origin/master
